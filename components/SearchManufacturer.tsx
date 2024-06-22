22141f94e8ea5d9bfadd4985b7c96744c87d1000
Daniel Nunes <danielnunes@MBP-de-Daniel.home>
2024-06-23 00:02:48 +0100

    add z-index to comboboxOptions

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -1,4 +1,4 @@
-"use client";
+'use client';
 
 import {
   Combobox,
@@ -7,68 +7,68 @@ import {
   ComboboxOptions,
   ComboboxOption,
   Transition,
-} from "@headlessui/react";
-import Image from "next/image";
-import { SearchManufacturerProps } from "@/types";
-import { manufacturers } from "@/constants";
+} from '@headlessui/react';
+import Image from 'next/image';
+import { SearchManufacturerProps } from '@/types';
+import { manufacturers } from '@/constants';
 
-import { useState, Fragment } from "react";
+import { useState, Fragment } from 'react';
 
 const SearchManufacturer = ({
   manufacturer,
   setManuFacturer,
 }: SearchManufacturerProps) => {
-  const [query, setQuery] = useState("");
+  const [query, setQuery] = useState('');
 
   const filteredManufacturers =
-    query === ""
+    query === ''
       ? manufacturers
       : manufacturers.filter((item) =>
           item
             .toLowerCase()
-            .replace(/\s+/g, "")
-            .includes(query.toLowerCase().replace(/\s+/g, ""))
+            .replace(/\s+/g, '')
+            .includes(query.toLowerCase().replace(/\s+/g, ''))
         );
 
   return (
-    <div className="search-manufacturer">
+    <div className='search-manufacturer'>
       <Combobox value={manufacturer} onChange={setManuFacturer}>
-        <div className="relative w-full">
+        <div className='relative w-full'>
           {/* Button for the combobox Click on the icon to see the complete dropdown */}
-          <ComboboxButton className="absolute top-[14px]">
+          <ComboboxButton className='absolute top-[14px]'>
             <Image
-              src="/car-logo.svg"
+              src='/car-logo.svg'
               width={20}
               height={20}
-              className="ml-4"
-              alt="car logo"
+              className='ml-4'
+              alt='car logo'
             />
           </ComboboxButton>
 
           {/* Input field for searching */}
           <ComboboxInput
-            className="search-manufacturer__input"
+            className='search-manufacturer__input'
             displayValue={(item: string) => item}
             onChange={(event) => setQuery(event.target.value)} // Update the search query when the input changes
-            placeholder="Volkswagen..."
+            placeholder='Volkswagen...'
           />
 
           {/* Transition for displaying the options */}
           <Transition
             as={Fragment} // group multiple elements without introducing an additional DOM node i.e., <></>
-            leave="transition ease-in duration-100"
-            leaveFrom="opacity-100"
-            leaveTo="opacity-0"
-            afterLeave={() => setQuery("")} // Reset the search query after the transition completes
+            leave='transition ease-in duration-100'
+            leaveFrom='opacity-100'
+            leaveTo='opacity-0'
+            afterLeave={() => setQuery('')} // Reset the search query after the transition completes
           >
             <ComboboxOptions
-              className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm"
+              className='absolute mt-1 z-20 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm'
               static
             >
-              {filteredManufacturers.length === 0 && query !== "" ? (
+              {filteredManufacturers.length === 0 && query !== '' ? (
                 <ComboboxOption
                   value={query}
-                  className="search-manufacturer__option"
+                  className='search-manufacturer__option'
                 >
                   Create "{query}"
                 </ComboboxOption>
@@ -78,7 +78,7 @@ const SearchManufacturer = ({
                     key={item}
                     className={({ active }) =>
                       `relative search-manufacturer__option ${
-                        active ? "bg-primary-blue text-white" : "text-gray-900"
+                        active ? 'bg-primary-blue text-white' : 'text-gray-900'
                       }`
                     }
                     value={item}
@@ -87,7 +87,7 @@ const SearchManufacturer = ({
                       <>
                         <span
                           className={`block truncate ${
-                            selected ? "font-medium" : "font-normal"
+                            selected ? 'font-medium' : 'font-normal'
                           }`}
                         >
                           {item}
@@ -98,8 +98,8 @@ const SearchManufacturer = ({
                           <span
                             className={`absolute inset-y-0 left-0 flex items-center pl-3 ${
                               active
-                                ? "text-white"
-                                : "text-pribg-primary-purple"
+                                ? 'text-white'
+                                : 'text-pribg-primary-purple'
                             }`}
                           ></span>
                         ) : null}
